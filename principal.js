b958e2de5faeddf7fa4e20c874398c024113ceb7
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2025-08-28 12:37:18 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -11,10 +11,32 @@ var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 //busca o elemento da altura e altera seu conteúdo
-var tdAltura = paciente.querySelector(".info-altura")
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 //cria variavel imc para fazer o cálculo e achar o imc do paciente
 var IMC = peso / (altura * altura);
-var tdIMC = paciente.querySelector(".info-imc");
+
+
+//valores booleanos
+var pesoValido = true;
+var alturaValida = true;
+
+
+//alerta caso peso e altura sejam inválidos
+if(peso <= 0 || peso >= 1000){
+    var pesoValido = false;
+    alert("Peso Inválido");
+}
+
+if(altura <= 0 || altura >= 3.00){
+    var AlturaValida = false;
+    alert("Altura Inválida!");
+}
+
+if(pesoValido && alturaValida){
+    var tdIMC = paciente.querySelector(".info-imc");
 tdIMC.textContent = IMC;
+}
+
+
