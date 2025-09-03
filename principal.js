f8472d0808080ce8ca7d2e4b887d6b970e74299a
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2025-09-03 09:43:02 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -6,37 +6,43 @@ titulo.textContent = "Lira Nutrição";
 
 //Esse vai criar variavel buscar id do primeiro paciente e acessa o peso do primeiro paciente
 
-var paciente = document.querySelector("#primeiro_paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-//busca o elemento da altura e altera seu conteúdo
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
 
-//cria variavel imc para fazer o cálculo e achar o imc do paciente
-var IMC = peso / (altura * altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    //busca o elemento da altura e altera seu conteúdo
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    //cria variavel imc para fazer o cálculo e achar o imc do paciente
+    var IMC = peso / (altura * altura);
 
 
-//valores booleanos
-var pesoValido = true;
-var alturaValida = true;
+    //valores booleanos
+    var pesoValido = true;
+    var alturaValida = true;
 
 
-//alerta caso peso e altura sejam inválidos
-if(peso <= 0 || peso >= 1000){
+    //alerta caso peso e altura sejam inválidos
+    if(peso <= 0 || peso >= 1000){
     var pesoValido = false;
     alert("Peso Inválido");
-}
+    }   
 
-if(altura <= 0 || altura >= 3.00){
+    if(altura <= 0 || altura >= 3.00){
     var AlturaValida = false;
     alert("Altura Inválida!");
+    }
+
+    if(pesoValido && alturaValida){
+    var tdIMC = paciente.querySelector(".info-imc");
+    tdIMC.textContent = IMC.toFixed(2);
+    }
 }
 
-if(pesoValido && alturaValida){
-    var tdIMC = paciente.querySelector(".info-imc");
-tdIMC.textContent = IMC;
-}
 
 
---
+++ teste.js
@@ -0,0 +1,5 @@
+let notas = [100,85,90, 5];
+
+for(var i = 0; i < notas.length; i++){
+    console.log(notas);
+}
